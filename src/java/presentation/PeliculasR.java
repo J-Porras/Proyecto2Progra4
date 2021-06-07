7c4c2b8a5769ae40ca16dd50ddb4f060fbffacd2
pgat3000 <pgat3000@gmail.com>
2021-06-07 16:30:31 -0600

    multipart agregado

--- src/java/presentation/PeliculasR.java
+++ src/java/presentation/PeliculasR.java
@@ -8,17 +8,32 @@ package presentation;
 import Data.Service.logic.Service;
 import java.util.List;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
-import javax.ws.rs.NotAcceptableException;
-import javax.ws.rs.NotFoundException;
-import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.NotAcceptableException;
+import javax.ws.rs.POST;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+import org.glassfish.jersey.media.multipart.FormDataParam;
+
+
+
+
 import peliculas.Logic.peliculas;
 
 
@@ -67,4 +82,32 @@ public class PeliculasR {
             throw new NotFoundException(); 
         }
     }
+    
+    @GET
+    @Path("{id}/image")
+    @Produces("image/png")
+    public Response getImge(@PathParam("id") String id) throws IOException {
+        File file = new File(location+id);
+        ResponseBuilder response = Response.ok((Object) file);
+        return response.build();
+    }   
+    
+    @POST
+    @Path("{id}/image")
+    @Consumes(MediaType.MULTIPART_FORM_DATA) 
+    
+    public void addImge(@PathParam("id") String id, @FormDataParam("image") InputStream imagenStream) {  
+        try{
+                int read = 0;
+                byte[] bytes = new byte[1024];
+
+                OutputStream out = new FileOutputStream(new File(location + id));
+                while ((read = imagenStream.read(bytes)) != -1){out.write(bytes, 0, read);}
+                out.flush();
+                out.close();
+            } catch (Exception ex) {
+                throw new NotAcceptableException(); 
+            }
+  
+}
 }
--- src/java/presentation/RegistroApplication.java
+++ src/java/presentation/RegistroApplication.java
@@ -1,9 +1,20 @@
 package presentation;
 
+import java.util.HashSet;
+import java.util.Set;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
 
 @ApplicationPath("api")
 public class RegistroApplication extends Application{
-    
+       @Override
+    public Set<Class<?>> getClasses() {
+
+        HashSet<Class<?>> classes = new HashSet<>();
+        classes.add(MultiPartFeature.class);
+        System.out.println("added multipart feature");
+        classes.add(PeliculasR.class);
+        return classes;
+    }   
 }
